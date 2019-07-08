18b70aa155ba69248ae5124504ad670c4ea2354c
PabloCabeza <pcabeza7@gmail.com>
2019-07-08 11:36:05 -0500

    Se corrige la redacción de algunos items

--- vista/instruccion.php
+++ vista/instruccion.php
@@ -11,36 +11,58 @@
 		<meta http-equiv="expires" content="07 de mayo de 2018"><!--Inicio de construcción de la página-->
 
 		<link rel="stylesheet" type="text/css" href="../css/EstilosVs_100.css"/>
-       	<link rel="stylesheet" type="text/css" media="(max-width: 800px)" href="css/MediaQuery_EstilosViajeSurAmerica.css">
+       	<link rel="stylesheet" type="text/css" media="(max-width: 800px)" href="../css/MediaQuery_EstilosVs_100.css">
        	<link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=RLato|Raleway:400|Montserrat|Indie+Flower|Caveat'> 
+
+		<script type="text/javascript" src="../javascript/Funciones_varias.js" ></script>
 	</head>	
 	<body>
-	    <!--titulo y menu principal-->
-		<?php include("modulos/header.html");?>
-		<div onclick= "">
-			<div id="Inicio">
-				<h2>Instrucciones</h2>						
-			</div>	
-		    <p class="Inicio_4">Preguntas</p>
+	    <header>
+			<?php include("modulos/header.html");?>
+		</header>
+		<div onclick="ocultarMenu()">
+			<h2>Instrucciones para utilizar la plataforma.</h2>	
+			<p class="Inicio_6">¿Como participar?</p>
 		    <ul>
-		        <li class="Inicio_2">Tienes dos minutos para responder cada pregunta.</li>
-		        <li class="Inicio_2">Las respuestas dentro de los primeros 30 seg suman la totalidad de los puntos, si respondes despues de este tiempo, los puntos ganados comenzarán a disminuir hasta tener un valor de cero puntos al cumplir el tiempo establecido.</li>
+		        <li class="Inicio_3">Solo pueden participar personas que se encuentren en Colombia, proximamente expandiremos la oferta a otros paises</li>
+		        <li class="Inicio_3">Debe estar registrado en la plataforma Versus_20, e iniciar sesión con su cuenta de usuario.</li>
+		        <li class="Inicio_3">Seleccione el tema de su prueba, los temas con la etiqueta "libre" no califican para otorgar premios y son gratuitos.</li>
+		        <li class="Inicio_3">Realize la consignación bancaria por 3.000 Pesos en el siguiente número de cuenta bancaria:</li>
+		        <ul class="Inicio_0">
+		        	<li>Corresponsal Bancolombia</li>
+		        	<li>Cuenta de ahorro</li>
+		        	<li>1301 7178677</li>
+		        </ul>
+		        <li class="Inicio_3">Registre el número de consignación bancaria en la plataforma Versus_20, y una vez seleccionada la prueba podra introducir el número de su consignación</li>
 		    </ul>
-		    <p class="Inicio_4">Prueba</p>
+			<p class="Inicio_6">¿Como jugar?</p>
 		    <ul>
-		        <li class="Inicio_2">Las pruebas constan de 10 preguntas relacionadas al tema que se haya seleccionado.</li>
-		        <li class="Inicio_2">Las pruebas se harán efectivas una vez se alacanze un minimo de 15 participante.</li>
-		        <li class="Inicio_2">Sino se alcanza el minimo de participantes para la fecha pautada se reintegrará el dinero de participación a quienes hayan realizado y registrado depositos.</li>
-		        <li class="Inicio_2">Una vez abierta una pregunta comienza a correr el tiempo que es tomado en cuenta para los resultados hasta concluir este, el tiempo de navegación en la aplicación que no es tomado en cuenta.</li>
-		        <li>Usted puede responder en varias sesiones sin afectar su tiempo de respuesta, siempre y cuando no abra una pregunta.</li>
-		        <li>SOlo es posible sumar puntos si acierta la respuesta en la primera respuesta que elija, luego ya no es nposible sumar puntos aún contestado la respuesta correcta.</li>
+		        <li class="Inicio_3">La temática de la plataforma plantea una prueba de un tema seleccionado por usted, y que abarca 10 preguntas de poca complejidad, en la que participan 20 usuarios por tres premios pagados en dinero.</li>
+		        <li class="Inicio_3">Se cuenta con dos minutos para responder cada pregunta.</li>
+		        <li class="Inicio_3">Las respuestas correctas dentro de los primeros 10 segundos suman la totalidad de los puntos, si respondes despues de este tiempo, los puntos comenzarán a disminuir hasta tener un valor de cero puntos al cumplir el tiempo establecido de la pregunta.</li>
+		        <li class="Inicio_3">Las pruebas se harán efectivas una vez se alla alcanzado la totalidad de 20 participantes.</li>
+		        <li class="Inicio_3">Las pruebas tienen apertura todos los viernes a las 06:00 am  y cierran el mismo dia a las 06:00 pm los resultados se publicaran inmediatamente despues del cierre de la misma.</li>
+		        <li class="Inicio_3">Al responder una prueba se daran resultados parciales de su actuación en la misma; solo al cierre (viernes 06:00 pm) se publicara una tabla con los resultados generales de todos los participantes en la prueba.</li>
+		        <li class="Inicio_3">Los resultados generales se publicarán inmediatamente despues del cierre de la misma.</li>
+		        <li class="Inicio_3">Sino se alcanza el minimo de participantes para la fecha de lanzamiento se reintegrará el dinero de participación a quienes hayan realizado y registrado depositos.</li>
+		        <li class="Inicio_3">Una vez abierta una pregunta comienza a correr el tiempo que es tomado en cuenta para los resultados, luego de ser respondida el cronometro para hasta que decida abrir la proxima preguntar.</li>
+		        <li class="Inicio_3">Solo es posible sumar puntos si acierta la respuesta en la primera respuesta que elija, luego ya no es posible sumar puntos aún contestado la respuesta correcta.</li>
 		    </ul>
-		    <p class="Inicio_4">Premiación</p>
+			<p class="Inicio_6">¿Y el premio?</p>
 		    <ul>
-		        <li class="Inicio_2">Existe un unico premio al participante con mayor puntaje.</li>
-		        <li class="Inicio_2">No existe la denominación de premio desierto, siempre abra un ganador.</li>
-		        <li class="Inicio_2">.</li>
+		        <li class="Inicio_3">Los participantes que alcancen los tres mayores puntajes recibirán un premio en efectivo distribuido de la siguiente manera.
+				</li>
+				<ul class="Inicio_0">
+					<li>1er Premio: $ 40.000</li>
+					<li>2do Premio: $ 7.000</li>
+					<li>3er Premio: $ 3.000</li>
+				</ul>
+		    	<li class="Inicio_3">Los ganadores pueden retirar su dinero inmediatamente con su cedula laminada en cualquier agencia de ApuestasCucuta.</li>
+		    	<li class="Inicio_3">Los resultados generales de la prueba son publicados en la plataforma, debe iniciar sesión en su cuenta para poder verlos.</li>
 		    </ul>
 		</div>
+	    <footer>
+	        <?php include("modulos/footer.php");?>
+	    </footer> 
    	</body>
 </html>
