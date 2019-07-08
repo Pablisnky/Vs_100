06e58621b1cda316bc645473ae5ed135b0b7a42c
PabloCabeza <pcabeza7@gmail.com>
2019-07-08 17:23:55 -0500

    Se comienza a seguir el archivo en el repositorio

---
+++ controlador/bloquearPreguntaDemo.php
@@ -0,0 +1,71 @@
+ <?php
+	include("../conexion/Conexion_BD.php");
+	
+	$Tema= $_GET["val_4"];//recibe el nombre del Tema desde bloqueo.js
+	$Pregunta= $_GET["val_5"];//recibe el ID_Pregunta desde bloqueo.js
+	$ID_PD= $_GET["val_6"];//recibe el ID_Pregunta desde bloqueo.js
+ 
+	// echo "Tema de la prueba= " .  $Tema . "<br>"; //Esto se puede ver en preguntaxxxx_00.php porque es una solicitud ajax 
+	// echo "ID_Pregunta= " . $Pregunta . "<br>";//Esto se puede ver en preguntaxxxx_00.php porque es una solicitud ajax
+	// echo "ID_PD= " . $ID_PD . "<br>"; //Esto se puede ver en preguntaxxxx_00.php porque es una solicitud ajax realizada en ese archivo
+
+
+
+		  // ------------------------------------------------------------------------------------------------------
+		  // -------------------------------------------------------------------------------------------------------
+		    //Se corrige la hora que entrega el sistema, para que trabaje con la hora nacional colombiana
+		    date_default_timezone_set('America/Bogota');
+		    $HoraServidorPHP =date("Y-m-d  H:i:s");
+		    // echo "Hora PHP de respuesta" . $HoraServidorPHP . "<br>";
+
+		    //CUando se trabaje en local se utiliza la funcion NOW() para introducir la hora respuesta de mysql
+		  // -------------------------------------------------------------------------------------------------------------
+		  // --------------------------------------------------------------------------------------------------------------
+
+
+	//se consulta que tipo de respuesta tiene esta pregunta
+	$Consulta= "SELECT * FROM respuestas_demo WHERE ID_Pregunta='$Pregunta' AND Tema= '$Tema' AND ID_PD ='$ID_PD' ORDER BY ID_Respuesta DESC LIMIT 1";
+   	$Recordset= mysqli_query($conexion, $Consulta);
+    $Verificar= mysqli_fetch_array($Recordset);
+	 // echo $Verificar["Correcto"];
+
+	 //Se restan 2.25 puntos por cada bloqueo.
+	$Castigo=2.25;
+	$puntoDescontado= -2.25;
+			
+	//si existe una respuesta incorrecta para esta pregunta.
+    if($Verificar["Correcto"] == "0"){
+	   	//Se consulta la hora en la que se realizó la pregunta para introducirlo nuevamente en la consulta $insertar
+	   	$Hora_Pregunta= $Verificar["Hora_Pregunta"];
+
+		$Restar="UPDATE participante_demo SET puntos= (puntos - $Castigo) WHERE ID_PD ='$ID_PD'";
+		mysqli_query($conexion,$Restar);
+
+		//se inserta en la BD que el participante respondio incorrectamente nuevamente
+		$insertar= "INSERT INTO respuestas_demo(ID_Pregunta, ID_PD, Tema, Correcto, Hora_Pregunta, Hora_Respuesta, puntoGanado) VALUES('$Pregunta', '$ID_PD', '$Tema', 0, '$Hora_Pregunta', '$HoraServidorPHP', '$puntoDescontado')";
+		mysqli_query($conexion,$insertar);
+		
+		echo "<h3>Nuevamente su respuesta fue equivocada, sera penalizado con 2,25 centesimas de sus puntos</h3>";
+	}
+			else if($Verificar["Correcto"] == "Sin_Respuesta"){//si no se habia respondido esta pregunta
+				$Restar="UPDATE participante_demo SET puntos= (puntos - $Castigo) WHERE ID_PD ='$ID_PD'";
+				mysqli_query($conexion,$Restar);
+
+		  //   	//se inserta en la BD que el participante respondio incorrectamente
+				// $insertar= "INSERT INTO respuestas(ID_Pregunta, ID_Participante, ID_PD, Tema, Correcto, Hora_Respuesta) VALUES('$Pregunta', '$participante','$ID_PD', '$Tema', 0 $HoraServidorPHP)";
+				// mysqli_query($conexion,$insertar);
+
+				//se actualiza en la BD la hora a la que respondio la pregunta
+				$Actualizar_5= "UPDATE respuestas_demo SET correcto = 0, Hora_Respuesta = '$HoraServidorPHP', puntoGanado= '$puntoDescontado'  WHERE ID_Pregunta='$Pregunta' AND Tema ='$Tema' AND ID_PD = '$ID_PD'";
+				mysqli_query($conexion,$Actualizar_5);
+
+			 	echo "<h3>Su respuesta fue equivocada, será penalizado con 2,25 centesimas de sus puntos.</h3>";
+			}	
+			else if($Verificar["Correcto"] == 1){// si existe una respuesta correcta
+				echo "<h3>Anteriormente usted respondio correctamente esta pregunta, pero ahora se ha equivocado, no tomaremos en cuenta su equivocación</h3>";
+			}
+?>
+			<!-- Se refresca la pagina -->
+			<a class="bloqueo" href="javascript:history.go(0)">Nuevo intento</a> 
+			<!-- <a class="bloqueo" href="javascript:void(document.getElementById('RespuestaPreguntas').style.display='none')">Nuevo intento</a> -->
+			
