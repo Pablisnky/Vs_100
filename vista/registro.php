3f44248e9161b076b7b99cc77ee52259bce57303
PabloCabeza <pcabeza7@gmail.com>
2019-07-08 09:48:33 -0500

    Se organiza identación código HTML

--- vista/registro.php
+++ vista/registro.php
@@ -1,4 +1,3 @@
-<!--Formulario de registro de usuarios, aqui se cargan los datos de los nuevos participantes.-->
 <?php
 	session_start(); //se crea una sesion llamada verifica, esta sesión es exigida cuando se entra en la pagina que recibe los datos del formulario de registro, para evitar que un usuario recarge la pagina que recibe y cargue los datos nuevamente a la BD
 	$verifica = 1906;  
@@ -7,7 +6,7 @@
 <!DOCTYPE html>
 <html lang="es">
 	<head>
-		<title>Vs_100 Registro</title>
+		<title>Versus_20 Registro</title>
 
 		<meta http-equiv="content-type"  content="text/html; charset=utf-8"/>
 		<meta name="description" content="Juego de preguntas sobre suramerica."/>
@@ -21,67 +20,42 @@
 		<link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=RLato|Raleway:400|Montserrat|Indie+Flower|Caveat'>
 
 		<script type="text/javascript" src="../javascript/Funciones_varias.js" ></script>
-		<script type="text/javascript" src="../javascript/validarFormularios.js" ></script>		
+		<script type="text/javascript" src="../javascript/validarFormularios.js" ></script>
+		<script type="text/javascript" src="../javascript/Funciones_Ajax.js" ></script>		
 	</head>	
 	<body onload= "autofocusRegistroGratis()">
 		<div class="Secundario">
-            <!--titulo y menu principal-->
-			<?php include("modulos/header.html");?>
+			<header>
+				<?php include("modulos/header.html");?>
+			</header>
 			<div onclick="ocultarMenu()""><!--ocultarMenu()-->
 				<div class="Inicio_2">
 					<h2>Registro de participantes</h2> 
+					<br>
 					<form action="registrarse.php" method="POST" name="registroGratis" onsubmit="return validar_01()">
                         <fieldset class="Afiliacion_4">
-						<input type="text" name="nombre" id="Nombre" placeholder="Nombre" onchange="return literal()" autocomplete="off"><!-- literal() se encuentra en validarFormulario.js -->
-						<input type="text" name="apellido" id="Apellido" placeholder="Apellido"  onchange="return literal_2()" autocomplete="off"><!-- literal_2() se encuentra en validarFormulario.js -->
-						<input type="text" name="cedula" id="Cedula" placeholder="Cedula"  onchange="" autocomplete="off">
-						<input type="text" name="telefono" id="Telefono" placeholder="Teléfono"  onchange="" autocomplete="off">
-					<!--	<select name="departamento" id="Departamento" onclick="">
-							<option>Departamento</option>
-                                    <option>Amazonas</option> 
-                                    <option>Antioquia</option> 
-                                    <option>Arauca</option>
-                                    <option>Atlántico</option>
-                                    <option>Bolívar</option>   
-                                    <option>Boyacá</option>
-                                    <option>Caldas</option> 
-                                    <option>Caquetá</option> 
-                                    <option>Casanare</option> 
-                                    <option>Cauca</option>
-                                    <option>Cesar</option>
-                                    <option>Chocó</option> 
-                                    <option>Córdoba</option>
-                                    <option>Cundinamarca</option>
-                                    <option>Distrito Capital</option>
-                                    <option>Guainía</option> 
-                                    <option>Guaviare</option>
-                                    <option>Huila</option> 
-                                    <option>La Guajira</option>
-                                    <option>Magdalena</option>
-                                    <option>Meta</option>
-                                    <option>Nariño</option>
-                                    <option>Norte de Santander</option> 
-                                    <option>Putumayo</option>
-                                    <option>Quindio</option> 
-                                    <option>Risaralda</option> 
-                                    <option>San Andres y Providencia</option> 
-                                    <option>Santander</option> 
-                                    <option>Sucre</option> 
-                                    <option>Tolima</option> 
-                                    <option>Valle del Cauca</option> 
-                                    <option>Vaupés</option> 
-                                    <option>Vichada</option>
-						</select> -->
-						<input type="email" name="correo" id="Correo" placeholder="e-mail" autocomplete="off" onchange="llamarCorreo();"><!--this.value hace referencia al texto que se escribio en el input-->
-							
-						<div id="recibir"></div><!--recibe por medio de ajax la respuesta de verificarCorreo.php-->				
-							
-						<input type="password" name="clave" id="Clave" placeholder="Contraseña" onchange="return literal_3()" autocomplete='off' onblur="quitarAnuncio()">
-					</fieldset>
-						<input type="submit" value="Enviar" style="margin-top: 6%;">
+                        	<legend>Datos personales</legend> 
+							<input type="text" name="nombre" id="Nombre" placeholder="Nombre" onchange="return literal()" autocomplete="off"><!-- literal() se encuentra en validarFormulario.js -->
+							<input type="text" name="cedula" id="Cedula" placeholder="Cedula" onchange="llamar_verificaCedula()" autocomplete="off">
+                        	<div class="contenedor_11" id="Mostrar_verificaCedula"></div><!-- recibe respuesta de ajax llamar_verificaCedula()-->
+							<input type="text" name="correo" id="Correo" placeholder="Correo electronico"  onchange="validarFormatoCorreo(); setTimeout(llamar_verificaCorreo,200);" autocomplete="off">
+                        	<div class="contenedor_11" id="Mostrar_verificaCorreo"></div><!-- recibe respuesta de ajax llamar_verificaCorreo()-->
+						</fieldset>         
+						<fieldset class="Afiliacion_4">
+							<legend>Datos de accceso a la plataforma</legend>  
+							<div>
+								<input type="password" name="clave" id="Clave" placeholder="Contraseña" onchange="llamar_verificaClave()">
+								<div class="contenedor_11" id="Mostrar_verificaClave"></div><!-- recibe respuesta de ajax llamar_verificaClave()-->
+								<input type="password" name="confirmarClave" id="ConfirmarClave" placeholder="Repetir contraseña">
+							</div>                               
+                        	<input type="submit" name="Registrarse" value="Registrarse" style=" display: block; width: 120px;"  onclick="return validar_01();">
+                    	</fieldset> 
 					</form>
 				</div>
 			</div>
 		</div>
+    	<footer>
+        	<?php include("modulos/footer.php");?>
+    	</footer>
 	</body>
 </html>
