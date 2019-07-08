83c23462de531d483b96772ec6109cd8e0beefaf
PabloCabeza <pcabeza7@gmail.com>
2019-07-08 14:08:07 -0500

    Se comienza a seguir en el repositorio

---
+++ vista/modulos/footer.php
@@ -0,0 +1,5 @@
+<!--Pie de página de todas las páginas que no estan dentro de una cuenta de usuario-->
+	<div class="piePagina_2">
+        <p class="piePagina">Desarrollado por:<b> horebi _ Estudio de software</b></p>
+        <p class="piePagina">Pamplona - Norte de Santander - Colombia</p>
+    </div> 
