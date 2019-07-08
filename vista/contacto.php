f3886276d2b73b739d7e9a421b8e79ea18028727
PabloCabeza <pcabeza7@gmail.com>
2019-07-08 14:10:44 -0500

    Se actualizó identación en código HTML

--- vista/contacto.php
+++ vista/contacto.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="es">
 	<head>
-		<title>Vs_100 Contactenos</title>
+		<title>Versus_20 Contactenos</title>
 
 		<meta http-equiv="content-type"  content="text/html; charset=utf-8"/>
 		<meta name="description" content="Juego de preguntas sobre suramerica."/>
@@ -21,30 +21,57 @@
         <link rel="stylesheet" type="text/css" media="(max-width: 800px)" href="../css/MediaQuery_EstilosVs_100.css"> 
         <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=RLato|Raleway:400|Montserrat|Indie+Flower|Caveat'>
 
-		<script type="text/javascript" src="../javascript/Funciones_varias.js" ></script>
-	</head>	
+		<script type="text/javascript" src="../javascript/Funciones_varias.js"></script>
+        <script type="text/javascript" src="../javascript/validarFormularios.js"></script>
+        <script type="text/javascript">
+            document.addEventListener("keydown", contar, false);//contar() se encuentra en Funciones_varias.js 
+            document.addEventListener("keyup", contar, false);//contar() se encuentra en Funciones_varias.js 
+            document.addEventListener("keydown", valida_Longitud, false);//valida_Longitud() se encuentra en Funciones_varias.js 
+            document.addEventListener("keyup", valida_Longitud, false);//valida_Longitud() se encuentra en 
+        </script>	
+    </head>	
 	<body onload="autofocusContacto()">
-            <!--titulo y menu principal-->
-			<?php include("modulos/header.html");?>
-            <div onclick= "ocultarMenu()">
-                <div class="contacto_2">
-                    <p class="Inicio_1">Sugerencias, recomendaciones y cualquier inquietud que puedas tener, hazla saber enviandonos un mensaje.</p>
-                </div>
-				<div class="Inicio_2">
-                    <h2>Contactenos</h2>
-                    <form action="recibeContacto.php" method="post" autocomplete="off" name="Contacto" id="contacto" onsubmit="">
-                        <fieldset class="Afiliacion_4">
-                            <input type="text" name="nombre" id="Nombre" placeholder="Nombre" />
-                            <input type="text" name="apellido" id="Apellido" placeholder="Apellido"/>
-                            <input type="email"  name="correo" id="Correo" placeholder="Correo electronico"/>
-                            <input type="text"  name="asunto" id="Asunto" placeholder="Asunto">
-                            <label class="etiqueta">Contenido</label>
-                            <textarea  name="contenido" id="Contenido"></textarea>
-
-                            <input type="submit" id="submitContacto" name="Enviar" value="enviar"  />
-                        </fieldset>        
-                    </form>
-                </div>
+        <header>
+            <?php include("modulos/header.html");?>
+        </header>
+        <div onclick= "ocultarMenu()">
+            <h2>Contactenos</h2>
+            <div class="contacto_2">
+                <p class="Inicio_1">Sugerencias, recomendaciones y cualquier inquietud que puedas tener, hazla saber enviandonos un mensaje.</p>
             </div>
-		</body>
+		    <div class="Inicio_2">
+                <form action="../controlador/recibeContacto.php" method="post" autocomplete="off" name="Contacto" id="contacto" onsubmit=" return validar_02()">
+                    <fieldset class="Afiliacion_4">
+                        <input type="text" name="nombre" id="Nombre" placeholder="Nombre" />
+                        <input type="text" name="apellido" id="Apellido" placeholder="Apellido"/>
+                        <input type="email"  name="correo" id="Correo" placeholder="Correo electronico"/>
+                        <label class="etiqueta">Contenido</label>
+                        <textarea class="contenido_2" name="contenido" id="Contenido"></textarea>
+                        <input class="contador" type="text" name="contador" id="Contador" value="500">
+
+                        <input type="submit" id="submitContacto" name="Enviar" value="enviar"  />
+                    </fieldset>        
+                </form>
+            </div>
+        </div>
+        <footer>
+            <?php include("modulos/footer.php");?>
+        </footer>
+	</body>
 </html>
+
+
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<script type="text/javascript">
+// Con este script se ajusta el textarea según se va escribiendo el contenido del mismo
+    var textarea_A = document.querySelector('textarea');
+    textarea_A.addEventListener('keydown', autosize);
+             
+    function autosize(){
+      var el = this;
+      setTimeout(function(){
+        el.style.cssText = 'height:100px; padding:0';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+      },0);
+}
+</script>
